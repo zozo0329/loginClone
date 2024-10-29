7ef12f5b53be5988aa662e05572a940f5361b400
Zo <161946731+zozo0329@users.noreply.github.com>
2024-10-29 15:13:25 +0800

    added sounds to other buttons and a's

--- main.js
+++ main.js
@@ -13,8 +13,20 @@ document.querySelector("#cloneThree").addEventListener("click", function () {
   document.querySelector("#cloneTwo").style.visibility = "visible";
 });
 const audio = new Audio(`sounds/Yo estoy puesto pa ti.mp3`);
+const pou = new Audio(`sounds/pou-sound-effect.mp3`);
 document.querySelector("#cloneTwo").addEventListener("click", function () {
   audio.play();
   document.querySelector(".cat img").style.display = "block";
   document.querySelector("main").style.display = "none";
+});
+document.querySelector(".other-btn a").addEventListener("click", function () {
+  pou.play();
+});
+document
+  .querySelector("#create-account")
+  .addEventListener("click", function () {
+    pou.play();
+  });
+document.querySelector(".create-page a").addEventListener("click", function () {
+  pou.play();
 });
